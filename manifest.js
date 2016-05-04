88810c6674857cbbc2468266f34f69639f7af99a
Michael Kühnel <m.kuehnel@micromata.de>
2016-05-04 22:20:54 +0200

    Adapt breaking changes from good 7.0.0

--- manifest.js
+++ manifest.js
@@ -59,16 +59,21 @@ const manifest = {
             plugin: {
                 register: 'good',
                 options: {
-                    reporters: [{
-                        reporter: require('good-console'),
-                        events: {
-                            response: '*',
-                            log: '*'
-                        },
-                        config: {
-                            format: 'YYYY-MM-DD/HH:mm:ss.SSS'
-                        }
-                    }]
+                    ops: {
+                        interval: 15000
+                    },
+                    reporters: {
+                        console: [{
+                            module: 'good-squeeze',
+                            name: 'Squeeze',
+                            args: [{
+                                log: '*',
+                                response: '*'
+                            }]
+                        }, {
+                            module: 'good-console'
+                        }, 'stdout']
+                    }
                 }
             }
         },

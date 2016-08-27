a1110b26663edea207d7d948aad31a0ea188890c
Michael Kühnel <m.kuehnel@micromata.de>
2016-08-27 23:00:46 +0200

    Update examples

--- server/api/bar.js
+++ server/api/bar.js
@@ -4,37 +4,24 @@ const SetupEndpoint = require('./setup/setup.js');
 
 module.exports = SetupEndpoint({
     name: 'bar',
-    urls: [
-        {
-            params: '/read',
-            requests: [{
-                method: 'GET',
-                response: '/json-templates/bar.json'
-            }]
-        },
-        {
-            params: '/update/{id}',
-            requests: [{
-                method: 'PUT',
-                response: {
-                    success: true
-                }
-            },
-            {
-                method: 'PATCH',
-                response: {
-                    success: true
-                }
-            }]
-        },
-        {
-            params: '/delete/{id}',
-            requests: [{
-                method: 'DELETE',
-                response: {
-                    success: true
-                }
-            }]
-        }
-    ]
+    urls: [{
+        params: '/read',
+        requests: [{
+            method: 'GET',
+            response: '/json-templates/bar.json'
+        }]
+    }, {
+        params: '/update/{id}',
+        requests: [{
+            method: ['PUT', 'PATCH'],
+            response: {
+                success: true
+            }
+        }, {
+            method: 'DELETE',
+            response: {
+                deleted: true
+            }
+        }]
+    }]
 });
--- server/api/foo.js
+++ server/api/foo.js
@@ -4,12 +4,10 @@ const SetupEndpoint = require('./setup/setup.js');
 
 module.exports = SetupEndpoint({
     name: 'foo',
-    urls: [
-        {
-            requests: [
-                { response: '/json-templates/foo.json' }
-            ]
-        }
-    ]/*,
+    urls: [{
+        requests: [
+            { response: '/json-templates/foo.json' }
+        ]
+    }]/*,
     statusCode: 401*/
 });

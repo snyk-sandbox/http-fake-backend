a42758f56bc465d97419bcb9fc793525ce26f3ad
Michael Kühnel <m.kuehnel@micromata.de>
2016-09-27 23:34:07 +0200

    Feature: Improve fakability of http status codes
    You now can define different status codes on request level in addition to defining errors on endpoint level.
    
    So different subroutes of one endpoint can return different HTTP status codes. Plus you can choose between:
    -  a status code with a self defined response
    -  a status code with a predefined error object provided by boom

---
+++ server/api/fakingStatusCodes.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const SetupEndpoint = require('./setup/setup.js');
+
+module.exports = SetupEndpoint({
+    name: 'statusCodes',
+    urls: [
+        {
+            params: '/boomError',
+            requests: [{
+                // Returns a 402 status code + an error message provided by boom:
+                // {
+                //   "error" : "Payment Required",
+                //   "statusCode" : 402
+                // }
+                statusCode: 402
+            }],
+
+        },
+        {
+            params: '/customError',
+            requests: [{
+                // Returns a HTTP status code 406 and a self defined response:
+                response: { error: true },
+                statusCode: 406
+            }],
+        },
+        {
+            params: '/regularResponse',
+            requests: [{
+                // Returns a 401 error provided by boom
+                // as defined on endpoint level
+                response: '/json-templates/anotherExample.json'
+            }],
+        }
+    ],
+    statusCode: 401
+});
